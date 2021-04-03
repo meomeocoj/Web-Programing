bdcec3b5d363efaeece2b4b86855bcc25a9b03a5
miasnguyenx <miasnguyenx@gmail.com>
2021-04-03 23:48:34 +0700

    refactor directory

---
+++ week3/Lab/Sale.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<font size=4 color="blue">
+<?php
+
+$today = date('1, F d, Y');
+print "Welcome on $today to our huge blowout sale! </font>";
+$month = date('m');
+$year = date('Y');
+$dayofyear = date('z');
+if ( $month == 12 && $year = 2001 ) {
+    $daysleft = ( 365 - $dayofyear + 10);
+    print "<br> There are $daysleft sales days left";
+}
+elseif ( $month == 01 && $year == 2002 ) {
+    if ( $dayofyear <= 10 ){
+        $daysleft = ( 10 - $dayofyear);
+        print "<br> There are $daysleft sales days left";
+    }else {
+        print "<br>Sorry, our sale is over.";
+    }
+
+}
+print"<br> Our sale Ends january 10, 2002";
+?>
+    
+</body>
+</html>
